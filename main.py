3cb976be233d2cc162b3c031443d197dba5ac633
Radina Staneva <radistaneva@gmail.com>
2014-05-10 20:17:37 +0300

    Make render_str global

--- main.py
+++ main.py
@@ -24,13 +24,16 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 			       autoescape = True)
 
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
 	self.response.out.write(*a, **kw)
     
     def render_str(self, template, **params):
-	t = jinja_env.get_template(template)
-	return t.render(params)
+	return render_str(template, **params)
 
     def render(self, template, **kw):
 	self.write(self.render_str(template, **kw))
