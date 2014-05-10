d69f84a190c78938e5fce01b2fa2c812d8a8c229
Radina Staneva <radistaneva@gmail.com>
2014-05-10 13:34:48 +0300

    Replace newlines with html <br> tags in post content

--- main.py
+++ main.py
@@ -44,6 +44,10 @@ class Post(db.Model):
     created = db.DateTimeProperty(auto_now_add = True)
     last_modified = db.DateTimeProperty(auto_now = True)
 
+    def render(self):
+	self._render_text = self.content.replace('\n', <br>)
+	return render_str("post.html", p = self)
+
 class Permalink(BlogHandler):
     def get(self, post_id):
 	print post_id
